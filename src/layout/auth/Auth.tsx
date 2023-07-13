c4fa03b09aa79ed59b95f573603a62b6e1e5ded6
AhmedAlawartany <ahmed.emad@nadsoft.net>
2023-07-13 09:23:29 +0300

    feat(reac-main) : add error boundary lib

--- src/layout/auth/Auth.tsx
+++ src/layout/auth/Auth.tsx
@@ -1,32 +1,35 @@
-import { memo, ReactNode } from "react";
-import styled from "styled-components";
-import { Header, Footer } from "../components";
+import { memo, ReactNode } from 'react';
+import styled from 'styled-components';
+import { Header, Footer } from '../components';
+import { ErrorBoundary } from 'react-error-boundary';
 
 interface AuthLayoutProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  min-width: 360px;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    min-width: 360px;
 `;
 
-const Content = styled("div")`
-  z-index: 1;
-  width: 100%;
-  flex-grow: 1;
-  max-width: 1280px;
-  margin: 0 auto;
+const Content = styled('div')`
+    z-index: 1;
+    width: 100%;
+    flex-grow: 1;
+    max-width: 1280px;
+    margin: 0 auto;
 `;
 
 export const AuthLayout = memo(({ children }: AuthLayoutProps) => {
-  return (
-    <Main>
-      <Header />
-      <Content className="flex justify-center items-center">{children}</Content>
-      <Footer />
-    </Main>
-  );
+    return (
+        <Main>
+            <Header />
+            <ErrorBoundary fallback={<div>Something went wrong</div>}>
+                <Content className="flex justify-center items-center">{children}</Content>
+            </ErrorBoundary>
+            <Footer />
+        </Main>
+    );
 });
